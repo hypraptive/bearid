0082f49f812c0ff0ae0931b6518a59669fdff76c
Mary <github@infinidom.com>
2020-08-11 17:40:00 -0700

    Added -xy, -max and --replace_path.

--- tools/downscale_images.py
+++ tools/downscale_images.py
@@ -2,6 +2,7 @@
 
 import sys
 import argparse
+from argparse import RawTextHelpFormatter
 import xml_utils as u
 import datetime
 from collections import defaultdict
@@ -12,27 +13,33 @@ from collections import defaultdict
 ##    extracts bc embeddings to write to new file
 ##------------------------------------------------------------
 def main (argv) :
-	parser = argparse.ArgumentParser(description='Downscale images, writes them to parallel \n\tdirectories and write new xml with updated content.',
-		formatter_class=lambda prog: argparse.HelpFormatter(prog,max_help_position=50))
+	parser = argparse.ArgumentParser(description='    Downscale images and writes them to parallel directories.\n    Also write new xml with updated content.\n\n    Example: \n\tdownscale_images -xy 640 480 -replace imageSource imageSourceTiny x.xml',
+		formatter_class=RawTextHelpFormatter)
+	grp = parser.add_mutually_exclusive_group ()
 	parser.add_argument ('files', nargs='+')
-	parser.add_argument ('-x_max', '--x_max', default=1500,
-		help='max width of image, defaults to 1500.')
-	parser.add_argument ('-y_max', '--y_max', default=2000,
-		help='max heigth of image, defaults to 2000.')
+	grp.add_argument ('-xy', '--xy_max', nargs=2, 
+		help='max x and y of image.')
+	grp.add_argument ('-max', '--max_area', default=30000,
+		help='max size of image.')
+	parser.add_argument ('-replace', '--replace_path', nargs=2, 
+		default=['', './'] , help='replace old with new.')
 	parser.add_argument ('-v', '--verbosity', type=int, default=1,
 		choices=[0, 1, 2, 3], help='')
 		# help="increase output verbosity"
 	u.set_argv (argv)
 	args = parser.parse_args()
 	u.set_verbosity (args.verbosity)
-	u.set_argv (argv)
 	u.set_filetype ('faces')
 	verbose = 0
 	if verbose > 0:
 		print("files: ", args.files)
-
+	if args.xy_max :
+		args.max_area = int (args.xy_max[0]) * int (args.xy_max[1])
+	# print ('max_area: ', args.max_area)
+	# print ('replace_path: ', args.replace_path)
+	# print ('replace_path: ', args.replace_path[0], args.replace_path[1])
 	for face_xml in args.files :
-		u.downscale_face_file (face_xml)
+		u.downscale_face_file (face_xml, args.max_area, args.replace_path)
 
 if __name__ == "__main__":
 	main (sys.argv)
