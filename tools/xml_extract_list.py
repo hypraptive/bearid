e873220c80ecd0c80432f5fecef1199ffdb3c614
Mary <github@infinidom.com>
2020-12-07 17:25:58 -0800

    Used to create a new xml from list of images and xml to retrieve content.

--- tools/xml_extract_list.py
+++ tools/xml_extract_list.py
@@ -1,10 +1,11 @@
-#! /usr/bin/python
+#! /usr/bin/python3
 
 import sys
 import argparse
 import xml_utils as u
 import datetime
 import pdb
+from argparse import RawTextHelpFormatter
 from collections import defaultdict
 
 ##------------------------------------------------------------
@@ -18,11 +19,13 @@ from collections import defaultdict
 ##   
 ##------------------------------------------------------------
 def main (argv) :
-	parser = argparse.ArgumentParser(description='\nReplicates list of images using new face data in argumentlist.  Ex: xml_extract_list -o auto_test.xml gold_test.xml allBears_faces.xml', 
-		formatter_class=lambda prog: argparse.HelpFormatter(prog,max_help_position=50))
+	parser = argparse.ArgumentParser(description='\n\tCreate new file using search file. Unmatched content will also be written out.\n\n\tUsage: xml_extract_from_xml <search_list_file> <new_sources>  \n\n\tEx: xml_extract_from_xml -o auto_test.xml gold_test.xml allBears_faces.xml', 
+		formatter_class=RawTextHelpFormatter)
     # parser.formatter.max_help_position = 50
-	parser.add_argument ('orig_file', help='file to replicate')
+	parser.add_argument ('search_file', help='file with search content')
 	parser.add_argument ('files', nargs='+')
+	parser.add_argument ('-filetype', '--filetype', default="chips",
+		help='type of xml file: <images,chips,faces,pairs> .')
 	parser.add_argument ('-o', '--output', default="",
 		help='Output filename. Defaults to "copy_<date><time>_"')
 	parser.add_argument ('--verbosity', type=int, default=1,
@@ -35,14 +38,13 @@ def main (argv) :
 	if not args.output :
 		args.output = datetime.datetime.now().strftime("copy_%Y%m%d_%H%M.xml")
 	if verbose > 0 :
-		print "output  : ", args.output
-		print 'orig file : ', args.orig_file
-		print 'files : ', args.files
+		print("output  : ", args.output)
+		print('orig file : ', args.orig_file)
+		print('files : ', args.files)
 	xml_files = u.generate_xml_file_list (args.files)
 	u.set_argv (argv)
-	u.set_exec_name  ('extract_list')
-	u.replicate_file ([args.orig_file], xml_files, args.output)
-
+	u.set_exec_name  (sys.argv[0])
+	u.replicate_file ([args.orig_file], xml_files, args.output, args.filetype)
 if __name__ == "__main__":
 	main (sys.argv)
 
