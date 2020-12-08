e9c707b91f17fbb7d05dc2d62055904ae9b96344
Mary <github@infinidom.com>
2020-12-07 18:31:12 -0800

    Updated function call and modified help messages.

--- tools/xml_extract_list.py
+++ tools/xml_extract_list.py
@@ -9,22 +9,22 @@ from argparse import RawTextHelpFormatter
 from collections import defaultdict
 
 ##------------------------------------------------------------
-##  xml_extract_list face_list_source *.xml/dirs
+##  xml_update_list face_list_source *.xml/dirs
 ##    given list of images, return same list of images with 
 ##		new data from argument list.
 ##      
 ##      Use in replicating a test set previous used for training
 ##
-##	ex: xml_extract_list -o auto_test.xml gold_test.xml  allBears_faces.xml
+##	ex: xml_update_list -o auto_test.xml gold_test.xml  allBears_faces.xml
 ##   
 ##------------------------------------------------------------
 def main (argv) :
-	parser = argparse.ArgumentParser(description='\n\tCreate new file using search file. Unmatched content will also be written out.\n\n\tUsage: xml_extract_from_xml <search_list_file> <new_sources>  \n\n\tEx: xml_extract_from_xml -o auto_test.xml gold_test.xml allBears_faces.xml', 
+	parser = argparse.ArgumentParser(description='\n\tCreate new file using original file updated with new sources. Unmatched content will also be written out.\n\n\tUsage: xml_update_from_xml <orig_file> <new_sources>  \n\n\tEx: xml_update_from_xml -o test.xml resize_test.xml', 
 		formatter_class=RawTextHelpFormatter)
     # parser.formatter.max_help_position = 50
-	parser.add_argument ('search_file', help='file with search content')
-	parser.add_argument ('files', nargs='+')
-	parser.add_argument ('-filetype', '--filetype', default="chips",
+	parser.add_argument ('orig_file', help='file with search content')
+	parser.add_argument ('new_sources', nargs='+')
+	parser.add_argument ('-filetype', '--filetype', default="faces",
 		help='type of xml file: <images,chips,faces,pairs> .')
 	parser.add_argument ('-o', '--output', default="",
 		help='Output filename. Defaults to "copy_<date><time>_"')
@@ -44,7 +44,7 @@ def main (argv) :
 	xml_files = u.generate_xml_file_list (args.files)
 	u.set_argv (argv)
 	u.set_exec_name  (sys.argv[0])
-	u.replicate_file ([args.orig_file], xml_files, args.output, args.filetype)
+	u.update_path ([args.orig_file], xml_files, args.output, args.filetype)
 if __name__ == "__main__":
 	main (sys.argv)
 
