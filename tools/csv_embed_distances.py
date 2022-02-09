a4d74e030e937647692ed122f3acafbbfbe2d908
Mary <github@infinidom.com>
2022-02-09 09:56:40 -0800

    Added options for - average : returns nearest label average (instead of individual), - top : returns top n closest - stats : write out collective matching stats for each label

--- tools/csv_embed_distances.py
+++ tools/csv_embed_distances.py
@@ -20,10 +20,16 @@ def main (argv) :
 		formatter_class=RawTextHelpFormatter)
 	parser.add_argument ('embed1')
 	parser.add_argument ('embed2')
+	parser.add_argument ('-av', '--average', action='store_true', 
+		help='for embed2, use average for each label instead of each embedding. Mutually exclusive with --top_match')
+	parser.add_argument ('-top', '--top_match', 
+		help='find closest n matches and use simple majority for matching. Defaults to 1.  Used mutually exclusively with --average.')
 	parser.add_argument ('-db', '--db',
 		help='db of images info.')
 	parser.add_argument ('-out', '--output', default="e_dist.csv",
 		help='specify csv output file.')
+	parser.add_argument ('-stats', '--stats', action='store_true', 
+		help='Write matching stats for each label.')
 	parser.add_argument ('-v', '--verbosity', type=int, default=1,
 		choices=[0, 1, 2, 3], help='')
 		# help="increase output verbosity"
@@ -32,15 +38,24 @@ def main (argv) :
 	u.set_verbosity (args.verbosity)
 	u.set_argv (argv)
 	u.set_filetype ('embeds')
-	verbose = 0
-	if verbose > 0:
-		print("files: ", args.files)
+	if args.verbosity > 0:
+		print("input files: ", args.embed1, args.embed2)
+	if args.average and args.top_match :
+		print ('options --average and --top_match are mutually exclusive.\n')
+		return
 	if os.path.exists (args.output) :
 		u.current_datetime ()
 		csv_filename = 'e_dist_' + u.current_datetime () + '.csv'
 		print ('CSV file exists, writing to ' +  csv_filename)
 		args.output = csv_filename
-	u.gen_embed_dist_csv ([args.embed1], [args.embed2], args.output, args.db)
+	if args.average :
+		top_match = 0
+	elif args.top_match :
+		top_match = args.top_match
+	else :
+		top_match = 1  # default value
+
+	u.gen_embed_dist_csv ([args.embed1], [args.embed2], args.output, args.db, int (top_match), args.stats)
 
 if __name__ == "__main__":
 	main (sys.argv)
